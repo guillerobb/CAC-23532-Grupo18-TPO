ac5a1df87bbcab91de36d2fce788869c4d5d3910
Juan Manuel <147455616+Juan09Ma@users.noreply.github.com>
2023-10-24 00:08:14 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let header= `
+<nav>
+<a href="index.html">Home</a>
+<a href="mundiales.html">Mundiales</a>
+<a href="jugadores.html">Jugadores</a>
+<a href="partidos.html">Partidos</a>
+<a href="about.html">Acerca de</a>
+<a href="contact.html">Contacto</a>
+<a href="gallery.html">Gallery</a>
+</nav>
+`
+
+document.getElementById("header").innerHTML= header;
+
+let tituloIndex = `
+<h1><img src="encabezado.jpg" border="1" width="100%" height="200"</h1>
+<h6>Sitio no oficial sobre la Selección Argentina de Fútbol</h6>
+`
+document.getElementById("tituloIndex").innerHTML= tituloIndex;
+
+let footer = `
+<p>Derechos Reservados @Grupo 18 FullStack Python Comisión 23532 &#169;2023</p>
+`
+document.getElementById("footer").innerHTML= footer;
+
+
